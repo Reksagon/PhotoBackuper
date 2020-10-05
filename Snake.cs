a21e50322b4256d74845d99cf26c4ec39e1785e3
kravamax <32734778+kravamax@users.noreply.github.com>
2020-10-05 21:52:52 +0300

    Create file for SnakeGame

---
+++ Snake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SnakeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Snake obj = new Snake();
+            obj.Show();
+            Console.ReadKey();
+        }
+
+    }
+    class Snake
+    {
+        public Snake() { }
+        public void Show()
+        {
+            Console.WriteLine("Hello, it's new game");
+        }
+    }
+}
